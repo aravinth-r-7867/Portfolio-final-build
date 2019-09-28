b701e3d14dafe53c2999e2fe928e7bc59aaf41c9
Aravinth Ramesh <aravinth.r@zohocorp.com>
2019-09-28 14:28:14 +0530

    [img.extension case changed]

--- app/components/image-gallery-grid.js
+++ app/components/image-gallery-grid.js
@@ -12,29 +12,29 @@ export default Component.extend({
             "first" : "marina-boat.JPG",
             "second" : "marina-sun-2.JPG ",
             "third" : "Arikkal-falls.JPG",
-            "fourth" : "Athirapally-falls.jpg",
+            "fourth" : "Athirapally-falls.JPG",
             "fifth" : "couples-in-marina.JPG",
-            "sixth" : "trichy-uchi-pillayar-kovil.jpg",
-            "seventh" : "srirangam-temple.jpg"
+            "sixth" : "trichy-uchi-pillayar-kovil.JPG",
+            "seventh" : "srirangam-temple.JPG"
             },
 
             {
-            "first" : "Rameshwaram-Pamban-bridge.jpg", 
+            "first" : "Rameshwaram-Pamban-bridge.JPG", 
             "second" : "kaveri-water.JPG",
-            "third" : "manali-buddha-temple.jpg",
+            "third" : "manali-buddha-temple.JPG",
             "fourth" : "Alleppey-boat.JPG",
-            "fifth" : "manali-stream.jpg",
+            "fifth" : "manali-stream.JPG",
             "sixth" : "manali-sun.JPG",
             "seventh" : "manali-tree.JPG"
             },
 
             {
             "first" : "kanyakumari-horse.JPG",
-            "second" : "Aluva-aquaduct-bridge.jpg ",
+            "second" : "Aluva-aquaduct-bridge.JPG ",
             "third" : "kanyakumari-boat.JPG",
             "fourth" : "srirangam-sculpture.JPG",
             "fifth" : "kanyakumari-sea-roaring.JPG",
-            "sixth" : "pitchavaram-sun.jpg",
+            "sixth" : "pitchavaram-sun.JPG",
             "seventh" : "marina-sun-1.JPG"
             }
 
